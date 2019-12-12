4e97199ac46ed125b282b47f5fa9f962670709e6
AamilShohail <shohail.17@itfac.mrt.ac.lk>
2019-12-12 16:17:18 +0530

    ImageUploadWebAPI

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,43 @@
+﻿using ImageUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ImageUpload.Controllers
+{
+    public class GalleryController : ApiController
+    {
+        [HttpPost]
+        [Route("api/UploadImageGallery")]
+        public HttpResponseMessage UploadImageGallery()
+        {
+            string imageName = null;
+            var httprequest = HttpContext.Current.Request;
+
+            var postedFile = httprequest.Files["Image"];
+            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+            var filePath = HttpContext.Current.Server.MapPath("~/Gallery/" + imageName);
+            postedFile.SaveAs(filePath);
+
+            using (ImageGalleryEntities1 db = new ImageGalleryEntities1())
+            {
+                Image image = new Image()
+                {
+                    ImageCaption = httprequest["ImageCaption"],
+                    ImageName = imageName,
+                    Category = httprequest["Category"],
+
+                };
+                db.Images.Add(image);
+                db.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using ImageUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ImageUpload.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [HttpPost]
+        [Route("api/UploadImage")]
+        public HttpResponseMessage UploadImage()
+        {
+            string imageName = null;
+            var httprequest = HttpContext.Current.Request;
+
+            var postedFile = httprequest.Files["Image"];
+            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+            var filePath = HttpContext.Current.Server.MapPath("~/Image/" + imageName);
+            postedFile.SaveAs(filePath);
+
+            using (ImageLibraryEntities db = new ImageLibraryEntities())
+            {
+                Image image = new Image()
+                {
+                    ImageCaption = httprequest["ImageCaption"],
+                    ImageName = imageName
+
+                };
+                db.Images.Add(image);
+                db.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
